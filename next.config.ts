a034c496b79667bbcd2d3a0301f845427d05a4d7
Nazril Acil <nazrilacil04@gmail.com>
2025-05-30 01:39:07 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains:
     ['lncugkvgbsouxezwgmdx.supabase.co']
